2dd737e78b1ac5dc484270b9c95807d253b1d762
taisoliva <tais15oliva@gmail.com>
2023-05-12 16:00:36 -0300

    Create e Read da tabela games

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -14,19 +14,23 @@ export async function postGames(req,res){
     const { name, image, stockTotal, pricePerDay } = req.body
     console.log(name, image, stockTotal, pricePerDay)
 
-    res.sendStatus(200)
+    try {
+        const nameCheck = await db.query(`SELECT * FROM games WHERE name='${name}';`)
+        console.log(nameCheck.rows.length)
+        if(nameCheck.rows.length !== 0) return res.status(409).send("Jogo já cadastrado")
 
-    /* try {
-        const user = await db.collection("users").findOne({ email })
-        if (user) return res.status(409).send("E-mail já foi cadastrado!")
-
-        const hash = bcrypt.hashSync(password, 10)
+        await db.query(`INSERT INTO games (name, image, "stockTotal", "pricePerDay") 
+                                    VALUES ('${name}', '${image}', ${stockTotal}, ${pricePerDay});`)
+        
+        res.sendStatus(201)
+        
+        /*const hash = bcrypt.hashSync(password, 10)
 
         await db.collection("users").insertOne({ name, email, password: hash })
-        res.sendStatus(201)
+        res.sendStatus(201) */
 
     } catch (err) {
         res.status(500).send(err.message)
-    } */
+    }
 
 }

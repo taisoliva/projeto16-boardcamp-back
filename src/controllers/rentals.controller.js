35167ac5f9790b9a5213eeee647f82f405d260c9
taisoliva <tais15oliva@gmail.com>
2023-05-13 13:24:45 -0300

    feat: Inserir e Listar alugueis

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -1,10 +1,37 @@
 import {db} from "../database/database.js"
+import dayjs from "dayjs"
 
 export async function getRentals(req,res){
 
     try{
-        const rentals = await db.query(`SELECT * FROM rentals`)
-        res.send(rentals.rows)
+        const rentals = await db.query(`SELECT rentals.*, customers.name AS "clientName", games.name AS "gameName" FROM rentals 
+        JOIN customers ON customers.id = rentals."customerId"
+        JOIN games ON games.id = rentals."gameId";`)
+
+        const formatteRentals = rentals.rows.map(rental => {
+            return {
+                id:rental.id,
+                customerId: rental.customerId,
+                gameId: rental.gameId,
+                rentDate: dayjs(rental.rentDate).format("YYYY-MM-DD"),
+                daysRented : rental.daysRented,
+                returnDate: rental.returnDate,
+                originalPrice: rental.originalPrice,
+                delayFee: rental.delayFee,
+                customer: { 
+                    id:rental.customerId,
+                    name: rental.clientName
+                },
+                game : {
+                    id:rental.gameId,
+                    name: rental.gameName
+                }
+            }
+        })
+        
+        res.send(formatteRentals)
+
+
 
     }catch(err){
         return res.status(500).send(err.message)
@@ -18,7 +45,28 @@ export async function postRentals(req,res){
     try{
         const clientCheck = await db.query(`SELECT * FROM customers WHERE id=$1;`, [customerId])
         if(clientCheck.rows.length === 0) return res.status(400).send("Cliente não existente")
+
+        const gameCheck = await db.query(`SELECT * FROM games WHERE id=$1;`, [gameId])
+        if(gameCheck.rows.length === 0) return res.status(400).send("Jogo não existente")
+
+        const stockTotal = gameCheck.rows[0].stockTotal
+        const pricePerDay = gameCheck.rows[0].pricePerDay
+
+        const rentals = await db.query(`SELECT * FROM rentals WHERE "gameId"=$1`, [gameId])
+        if(rentals.rows.length === stockTotal) return res.status(400).send("Unidade não disponível")
+
+        const today = dayjs().format("YYYY-MM-DD")
+        const originalPrice = pricePerDay * daysRented
         
+        await db.query(`INSERT INTO rentals 
+                            ("customerId", "gameId", 
+                            "rentDate", "daysRented", 
+                            "returnDate", "originalPrice", "delayFee")
+                        VALUES ($1, $2, $3, $4, $5, $6, $7)`, 
+                        [customerId, gameId, today, daysRented, null, originalPrice, null])
+
+        res.sendStatus(201)
+
     }catch(err){
         return res.status(500).send(err.message)
     }
--- src/schemas/rentals.schema.js
+++ src/schemas/rentals.schema.js
@@ -1,5 +1,7 @@
 import joi from "joi"
 
 export const rentalsSchema = joi.object({
-    customerId: joi.number().greater(0)
+    customerId: joi.number().integer().greater(0).required(),
+    gameId: joi.number().integer().greater(0).required(),
+    daysRented: joi.number().integer().greater(0).required()
 })
